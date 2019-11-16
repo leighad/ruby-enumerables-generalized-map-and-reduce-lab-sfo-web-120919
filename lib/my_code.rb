a316c853b3c3a9605fdbe310a71adb5b6b0dc174
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-16 22:38:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,38 @@
-# Your Code Here
+# # Your Code Here
+def map(array)
+  # array.map do
+  #   |el| el * -1
+  #   end
+  # (map(array){|n| n * -1})
+  new = []
+  i = 0 
+  while i < array.length do 
+    
+    new << yield
+    i += 1 
+  end
+  return new 
+end
+
+# my_own_map([1, 2, 3, -9]) { |n| n * -1 }
+
+# def my_own_map(array)
+#   # return array.map {|n| n}
+# end
+
+# def my_own_map(array)
+#   # return array.map {|n| n * 2}
+# end
+
+# def my_own_map(array)
+#   # return array.map {|n| n * n}
+# end
+
+# # def my_own_map(array)
+# #   array.reduce {|n| }
+# # end
+
+# def my_own_map(array)
+#   array.map { |n| n * -1 }
+# end
+
