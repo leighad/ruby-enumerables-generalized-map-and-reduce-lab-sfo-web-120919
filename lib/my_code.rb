8aba5218166141599152bcc84758ea8c59319893
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-16 23:35:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+# # Your Code Here
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length do 
+    new << yield(array[i])
+    i += 1 
+  end
+  new 
+end
+
+def reduce(array, start = nil)
+  if start
+    total = start
+    i = 0 
+  else
+    total = array[0]
+    i = 1 
+  end
+  while i < array.length do 
+    total = yield(total, array[i])
+    i += 1 
+  end
+  total
+end
+
+
